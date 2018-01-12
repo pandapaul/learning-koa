d0deba743ba6bf9c51c0a533392aef04c9ac5188
Paul Anderson <paul.anderson@uberops.com>
2018-01-12 08:51:55 -0500

    :sparkles: solution to exercise 2

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const koa = require('koa')
 
 const app = new koa()
-const port = 
+const port = +process.argv[2] || 5000 
 
 app.use(ctx => {
-  ctx.body = 'hello'
+  if (ctx.path === '/') {
+    ctx.body = 'hello koa'
+  } else if (ctx.path === '/404') {
+    ctx.body = 'page not found'
+  } else if (ctx.path === '/500') {
+    ctx.body = 'internal server error'
+  }
 })
-app.listen(+process.argv[2] || 5000)
+app.listen(port)
+console.log(`Listening on port ${port}`)
