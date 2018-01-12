8a9ae089f4b0ab1f4ca4446d1f2ce1b6606ce1fb
Paul Anderson <paul.anderson@uberops.com>
2018-01-12 09:51:17 -0500

    :sparkles: solution to exercise 3

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-const koa = require('koa')
+const Koa = require('koa')
+const bodyparser = require('koa-bodyparser')
 
-const app = new koa()
-const port = +process.argv[2] || 5000 
+const app = new Koa()
+const port = +process.argv[2] || 5000
 
+app.use(bodyparser())
 app.use(ctx => {
   if (ctx.path === '/') {
-    ctx.body = 'hello koa'
+    ctx.body = (ctx.request.body.name || '').toUpperCase()
   } else if (ctx.path === '/404') {
     ctx.body = 'page not found'
   } else if (ctx.path === '/500') {
