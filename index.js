6396f96fe79fa8aca10d474e41f49bfd007b8571
Paul Anderson <paul.anderson@uberops.com>
2018-01-12 10:06:15 -0500

    :sparkles: solution to exercise 4

--- index.js
+++ index.js
@@ -1,18 +1,30 @@
 const Koa = require('koa')
 const bodyparser = require('koa-bodyparser')
+const route = require('koa-route')
+const fs = require('fs')
 
 const app = new Koa()
 const port = +process.argv[2] || 5000
 
 app.use(bodyparser())
-app.use(ctx => {
-  if (ctx.path === '/') {
-    ctx.body = (ctx.request.body.name || '').toUpperCase()
-  } else if (ctx.path === '/404') {
-    ctx.body = 'page not found'
-  } else if (ctx.path === '/500') {
-    ctx.body = 'internal server error'
-  }
-})
+
+app.use(route.get('/', ctx => {
+  ctx.body = 'hello koa'
+}))
+app.use(route.post('/', ctx => {
+  ctx.body = (ctx.request.body.name || '').toUpperCase()
+}))
+app.use(route.get('/stream', ctx => {
+  ctx.body = fs.createReadStream(process.argv[3])
+}))
+app.use(route.get('/json', ctx => {
+  ctx.body = { foo: 'bar' }
+}))
+app.use(route.get('/404', ctx => {
+  ctx.body = 'page not found'
+}))
+app.use(route.get('/500', ctx => {
+  ctx.body = 'internal server error'
+}))
 app.listen(port)
 console.log(`Listening on port ${port}`)
